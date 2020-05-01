fc5f938d43dca55edf106e57a9c47d939b638e1a
Brad Trantham <btrantham@swri.org>
2020-05-01 15:26:20 -0500

    Commit after lecture 163

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { FlatList,Text } from 'react-native'
+import { FlatList } from 'react-native'
 import { useSelector } from 'react-redux'
+
+import ProductItem from '../../components/shop/ProductItem'
 
 const ProductOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts)
     // in older versions of react you'd also need to define the keyExtractor for the FlatList
-    return <FlatList data={products} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
+    return <FlatList data={products} renderItem={itemData => 
+        <ProductItem image={itemData.item.imageUrl} title={itemData.item.title} price={itemData.item.price} 
+                     onViewDetail={()=>{
+                         props.navigation.navigate('ProductDetail', {productId: itemData.item.id, 
+                                                                     productTitle: itemData.item.title})
+                     }} 
+                     onAddToCart={()=>{}} />} />
+}
+
+ProductOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
 }
 
 export default ProductOverviewScreen
