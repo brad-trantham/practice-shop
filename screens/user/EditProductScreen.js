f9c36c55368ce62c7fc20ebd169ae851d55ace40
Brad Trantham <btrantham@swri.org>
2020-05-29 17:00:48 -0500

    Commit after lecture 189

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,58 +1,113 @@
-import React, {useState, useEffect, useCallback} from 'react'
-import { View, ScrollView, Text, TextInput, StyleSheet, Platform } from 'react-native'
+import React, {useEffect, useCallback, useReducer} from 'react'
+import { View, ScrollView, Text, TextInput, StyleSheet, Platform, Alert } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import HeaderButton from '../../components/UI/HeaderButton'
 import * as productActions from '../../store/actions/products'
 
+const FORM_INPUT_UPDATE = 'UPDATE'
+
+const formReducer = (state, action) => {
+    if(action.type === FORM_INPUT_UPDATE) {
+        const updatedValues = {
+            ...state.inputValues,
+            [action.input]: action.value
+        }
+
+        const updatedValidities = {
+            ...state.inputValidities,
+            [action.input]: action.isValid
+        }
+
+        let updatedFormIsValid = true
+        for(const key in updatedValidities) {
+            updatedFormIsValid = updatedFormIsValid && updatedValidities[key]
+        }
+
+        return {formIsValid: updatedFormIsValid, 
+                inputValues: updatedValues, 
+                inputValidities: updatedValidities}
+    }
+    return state
+}
+
 const EditProductScreen = props => {
     const prodId = props.navigation.getParam('productId')
     const editedProduct = useSelector(state => state.products.userProducts.find(prod => prod.id === prodId))
     const dispatch = useDispatch()
 
-    const[title, setTitle] = useState(editedProduct ? editedProduct.title : '')
-    const[imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '')
-    const[price, setPrice] = useState('')
-    const[description, setDescription] = useState(editedProduct ? editedProduct.description : '')
+const [formState, dispatchFormState] = useReducer(formReducer, {
+        inputValues: {
+            title: editedProduct ? editedProduct.title : '',
+            imageUrl: editedProduct ? editedProduct.imageUrl : '',
+            description: editedProduct ? editedProduct.description : '',
+            price: ''
+        }, 
+        inputValidities: {
+            title: editedProduct ? true : false,
+            imageUrl: editedProduct ? true : false,
+            description: editedProduct ? true : false,
+            price: editedProduct ? true : false,
+        },
+        formIsValid: editedProduct ? true : false
+    })
 
     const submitHandler = useCallback(() => {
+        if(!formState.formIsValid) {
+            Alert.alert('Wrong input!', 'Please check the errors in the form', [{text: 'Okay'}])
+            return
+        }
         if(editedProduct){
-            dispatch(productActions.updateProduct(prodId, title, description, imageUrl))
+            dispatch(productActions.updateProduct(prodId, formState.inputValues.title, formState.inputValues.description, formState.inputValues.imageUrl))
         }
         else {
             // the + converts the price from a string to a number
-            dispatch(productActions.createProduct(title, description, imageUrl, +price))
+            dispatch(productActions.createProduct(formState.inputValues.title, formState.inputValues.description, formState.inputValues.imageUrl, +formState.inputValues.price))
         }
         props.navigation.goBack()
-    }, [dispatch, prodId, title, description, imageUrl, price])
+    }, [dispatch, prodId, formState])
 
     useEffect(() => {
         props.navigation.setParams({submit: submitHandler})
     }, [submitHandler])
+
+    const textChangeHandler = (inputIdentifier, text) => {
+        let isValid = false
+        if(text.trim().length > 0) {
+            isValid = true
+        }
+
+        dispatchFormState({type: FORM_INPUT_UPDATE, value: text, isValid, input: inputIdentifier})
+    }
 
     return (
     <ScrollView>
         <View style={styles.form}>
             <View style={styles.formControl}>
                 <Text style={styles.label}>Title</Text>
-                <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
+                {// the text argument is passed by default and isn't bound
+                }
+                <TextInput style={styles.input} value={formState.inputValues.title} onChangeText={textChangeHandler.bind(this, 'title')}
+                           keyboardType='default' autoCapitalize='sentences' autoCorrect returnKeyType='next'/>
+                {!formState.inputValidities.title && <Text>Please enter a valid title.</Text>}
             </View>
             <View style={styles.formControl}>
                 <Text style={styles.label}>Image URL</Text>
-                <TextInput style={styles.input} value={imageUrl} onChangeText={text => setImageUrl(text)}/>
+                <TextInput style={styles.input} value={formState.inputValues.imageUrl} onChangeText={textChangeHandler.bind(this, 'imageUrl')}/>
             </View>
             {
             // only allow price editing in add mode
             editedProduct ? null : 
             <View style={styles.formControl}>
                 <Text style={styles.label}>Price</Text>
-                <TextInput style={styles.input} value={price} onChangeText={text => setPrice(text)}/>
+                <TextInput style={styles.input} value={formState.inputValues.price} onChangeText={textChangeHandler.bind(this, 'price')}
+                           keyboardType='decimal-pad'/>
             </View>
             }
             <View style={styles.formControl}>
                 <Text style={styles.label}>Description</Text>
-                <TextInput style={styles.input} value={description} onChangeText={text => setDescription(text)}/>
+                <TextInput style={styles.input} value={formState.inputValues.description} onChangeText={textChangeHandler.bind(this, 'description')}/>
             </View>
         </View>
     </ScrollView>
